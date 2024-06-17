a060efbb1bd51e0c28bdd281956f6f90dd98a78b
Ore Adeleye <oreadeleye67@gmail.com>
2024-06-17 14:27:09 +0100

    Update active_cname.js

--- main/active_cname.js
+++ main/active_cname.js
@@ -36,7 +36,7 @@ module.exports = {
   'xyzblog': 'xyzscratcher.github.io',
   '13': '0x3st.netlify.app',
   'leiloser': 'leiloser.github.io',
-  'foryou': 'foryou-byfw.vercel.app', //noCF
+  'foryou': 'foryou-byfw.vercel.app', // noCF
   'appcheck': 'app-checker.github.io',
   'json': 'waifu-project.github.io',
   'spin': 'rahulkarda.github.io',
@@ -63,7 +63,7 @@ module.exports = {
   'blockchain': 'willin.github.io',
   'antiadblock': 'js-cool.github.io',
   'noho': 'willin.github.io',
-  //'jen': 'dirkhe1051931999.github.io',
+  // 'jen': 'dirkhe1051931999.github.io',
   'uiw': 'uiwjs.github.io',
   'shu': '87a5f664-7619-4689-b0d8-3cf0d028a6d3.id.repl.co',
   'exam': 'examined.netlify.app',
@@ -72,9 +72,9 @@ module.exports = {
   'xiaomeiwu': 'xiaomeiwu.github.io',
   'xiaopi': 'xiaopi.netlify.app',
   'xiaowang': 'xiaowang.netlify.app',
-  'love': 'sincere.vercel.app',// noCF
+  'love': 'sincere.vercel.app', // noCF
   'ip': 'vercel-dns.kai.bi', // noCF
-  'mingyan': 'lehs8n.coding-pages.com', //noCf
+  'mingyan': 'lehs8n.coding-pages.com', // noCf
   'na': 'qq.mcust.cn',
   'rx': 'willin.github.io',
   'yogg': 'yogsuhrawn.github.io',
@@ -106,9 +106,9 @@ module.exports = {
   'god': 'godwhitetaiwan.github.io',
   'godwhite': 'godwhitetaiwan.github.io',
   'lzscxb': 'lzscxb.github.io',
-  'fox': 'cname-china.vercel-dns.com', //noCF
+  'fox': 'cname-china.vercel-dns.com', // noCF
   'luca': 'lucatolton.github.io',
-  'notapi': 'notapi.vercel.app', //noCF
+  'notapi': 'notapi.vercel.app', // noCF
   'heiguoblog': 'fairy-plus.github.io',
   'zenobia': 'zenobiadevelopment.github.io',
   'ngcchinl': 'zeusgangzz.github.io',
@@ -117,10 +117,10 @@ module.exports = {
   'connor': 'connordoesdev.github.io',
   'mew': 'meowier.github.io',
   'ethan': 'ethan-phu.github.io',
-  'wzx': 'wzx-blog.vercel.app', //noCF
+  'wzx': 'wzx-blog.vercel.app', // noCF
   'eleven': 'elevenvac.github.io',
-  'wsee': 'md-wz.vercel.app', //noCF
-  'wlcf': 'md-wz.vercel.app', //noCF
+  'wsee': 'md-wz.vercel.app', // noCF
+  'wlcf': 'md-wz.vercel.app', // noCF
   'spicydevs': 'spicydevs.pages.dev',
   'cdn.spicydevs': 'spicydevs.github.io',
   'sd': 'spicydevs.pages.dev',
@@ -128,65 +128,64 @@ module.exports = {
   'minecraft': 'mcjs.pages.dev',
   'servers': 'opaayush.github.io',
   'world': 'pythoniaweb.github.io',
-  'frank': 'joyfrank.vercel.app',// noCF
+  'frank': 'joyfrank.vercel.app', // noCF
   'class20': 'redblueidea.github.io',
   'radio': 'ayahchill.github.io',
-  'start': 'start-pages.vercel.app',//noCF
-  'zx': 'cname.vercel-dns.com', //noCF
-  'aqiu': 'aqiu.vercel.app',//nocF
-  'scripthouse': 'scripthouse.vercel.app',//nocF
-  'elvis': 'elvisndubuisi.vercel.app',//nocF
-  'dinglin': 'roaring-churros-e65865.netlify.app',// noCF
+  'start': 'start-pages.vercel.app', // noCF
+  'zx': 'cname.vercel-dns.com', // noCF
+  'aqiu': 'aqiu.vercel.app', // nocF
+  'scripthouse': 'scripthouse.vercel.app', // nocF
+  'elvis': 'elvisndubuisi.vercel.app', // nocF
+  'dinglin': 'roaring-churros-e65865.netlify.app', // noCF
   'thitit': 'portfolio-five-tan.vercel.app',
   'duck': 'deng-duck.github.io',
   'ms': 'ms27520.github.io',
-  'zzy': 'hitptep.github.io',//noCF
-  'abe': 'hitptep.github.io',//noCF
-  'dice': 'cname.vercel-dns.com', //noCF
+  'zzy': 'hitptep.github.io', // noCF
+  'abe': 'hitptep.github.io', // noCF
+  'dice': 'cname.vercel-dns.com', // noCF
   'willmo': 'willmo.netlify.app',
   'navigator': 'navigator.github.io',
   'zswangziye': 'zswangziye.github.io',
   'duckwaline': 'waline-3bg5otthj-deng-duck.vercel.app',
   'dengduck': 'dengduck.netlify.app',
   'waio': 'cname.vercel-dns.com', // noCF
-  'joy': 'frankjoy.vercel.app',// noCF
-  'journey': 'feynmanfrank.github.io',// noCF
+  'joy': 'frankjoy.vercel.app', // noCF
+  'journey': 'feynmanfrank.github.io', // noCF
   'dhruvil': 'dhruvilmoradiya.github.io',
-  'whatsme.dhruvil': 'whatsme.netlify.app'
+  'whatsme.dhruvil': 'whatsme.netlify.app',
   'ziscul': 'ziscul.vercel.app',
   'muyangplus': 'muyangplus.github.io', // noCF
   'eshexon': 'eshexon-docs.netlify.app', // noCF
   'feat': 'featyou.vercel.app',
   'feat-status': 'status.cron-job.org',
   'zennnnnnnnnnnn': 'zennnnnnnnnnnn.github.io',
-  'xlenco'：'cname.vercel-dns.com', //noCF
-  'yxlr': 'regal-gnome-0c8689.netlify.app', //noCF
-  'vishal': 'vishalpx.github.io', //noCF
-  'gtcblog'：'gtcblog.netlify.app', //noCF
-  'willmocms': 'willmocms.vercel.app',//noCF
-  'syr': 'cname-china.vercel-dns.com',//noCF
+  'xlenco': 'cname.vercel-dns.com', // noCF
+  'yxlr': 'regal-gnome-0c8689.netlify.app', // noCF
+  'vishal': 'vishalpx.github.io', // noCF
+  'gtcblog': 'gtcblog.netlify.app', // noCF
+  'willmocms': 'willmocms.vercel.app', // noCF
+  'syr': 'cname-china.vercel-dns.com', // noCF
   'chirag-adhvaryu': 'chirag-adhvaryu.github.io',
   'marzm': 'xiaomait.github.io',
   'jayg': 'jayggit.github.io',
   'yashh-chauhan': 'yashh-chauhan.github.io',
   'lan': 'LanHuang025.github.io',
   'geekswg': 'geekswg.github.io',
-  'geek-waline': 'geekswg.vercel.app', //noCF
+  'geek-waline': 'geekswg.vercel.app', // noCF
   'fun': 'michaelsynan.github.io',
   'zmk': 'wszmk.github.io',
   'ba': 'mianba.netlify.app',
   'viren-trivedi': 'viren-trivedi.github.io',
-  'hajeekn': 'cname.vercel-dns.com', //noCF
+  'hajeekn': 'cname.vercel-dns.com', // noCF
   'yash-poojara': 'yash-poojara.github.io',
   'ajit-thakor': 'ajit-thakor.github.io',
   'viral-gadhiya': 'viral-gadhiya.github.io',
   'dudani-meet': 'dudani-meet.github.io',
-  'priti-shide': 'priti-shide.github.io'
+  'priti-shide': 'priti-shide.github.io',
   'ansari-saify': 'ansari-saify.github.io',
   'mihir-patel-agc': 'mihir-patel-agc.github.io',
   'ankit-acquaintsoft': 'ankit-acquaintsoft.github.io',
   'bhavesh-acquaintsoft': 'bhavesh-acquaintsoft.github.io',
-  'chirag-adhvaryu': 'chirag-adhvaryu.github.io',
   'insight': 'insight.adaptable.app',
   'lakhani-palak': 'lakhani-palak.github.io',
   'prajapati-divya': 'prajapati-divya.github.io',
@@ -206,5 +205,6 @@ module.exports = {
   '@': 'js-cool.github.io',
   'www': 'kv6xcc.coding-pages.com', // 国内镜像
   'logo': 'js-cool.github.io',
-  'thetechnikfreak': 'thetechnikfreak.netlify.app'
+  'thetechnikfreak': 'thetechnikfreak.netlify.app',
+  'ore': 'ore-codes.gihub.io'
 };
